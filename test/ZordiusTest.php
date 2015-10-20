331b32ebcff9697b8d82b2311e1c737963dafe61
Matt Kynaston <matt@claritum.com>
2015-10-20 14:52:49 +0100

    Ah, phpunit data providers... that's how it's done :)

--- test/ZordiusTest.php
+++ test/ZordiusTest.php
@@ -8,127 +8,102 @@ namespace KynxTest\V8js;
 use Kynx\V8js\Handlebars;
 use PHPUnit_Framework_Assert as Assert;
 use PHPUnit_Framework_AssertionFailedError as AssertionFailedError;
-use PHPUnit_Framework_Test as Test;
+use PHPUnit_Framework_TestCase as TestCase;
 use PHPUnit_Framework_TestResult as TestResult;
 use PHP_Timer as Timer;
 
 /**
- * Class to test against zorbius' HandlebarsTest fixtures
+ * Class to test against zordius' HandlebarsTest fixtures
  *
  * To use this you'll need to `git clone https://github.com/zordius/HandlebarsTest` and do a:
- *     cp /path/to/HandlebarsTest/fixtures/*.{json,txt,tmpl} tests/zordius
+ *     cp /path/to/HandlebarsTest/fixture/*.{json,txt,tmpl} tests/zordius
  *
  */
-class ZordiusTest implements Test
+class ZordiusTest extends TestCase
 {
-    protected $tests = [];
+    /**
+     * @var Handlebars
+     */
+    protected $hb;
     protected $allowArrayObjectTypes = false;
 
-    public function __construct($zordiusDir = false)
+    public function setUp()
     {
-        if ($zordiusDir === false) {
-            $zordiusDir = __DIR__ . '/zordius';
-        }
-        $this->tests = glob($zordiusDir . '/*.json');
+        $dirName = __DIR__ . '/zordius';
         $this->allowArrayObjectTypes = getenv('ALLOW_ARRAY_OBJECT_TYPES');
         $handlebarsSource = file_get_contents(dirname(__DIR__) . '/components/handlebars/handlebars.js');
         Handlebars::registerHandlebarsExtension($handlebarsSource);
+
+        $this->hb = new Handlebars();
+        $this->hb->registerPartial([
+            'partial1' => file_get_contents($dirName . '/partial001.tmpl'),
+            'partial001' => file_get_contents($dirName . '/partial001.tmpl'),
+            'partial2' => file_get_contents($dirName . '/partial002.tmpl'),
+            '001-simple-vars' => file_get_contents($dirName . '/001-simple-vars.tmpl'),
+            '017-hb-with' => file_get_contents($dirName . '/017-hb-with.tmpl')
+        ]);
+        $this->hb->registerHelper('helper1', "function (url, text) {
+            return '<a href=\"' + url + '\">' + text + '</a>';
+        }");
+        $this->hb->registerHelper('helper2', "function (options) {
+            return '<a href=\"' + options.hash.url + '\">' + options.hash.text + '</a>(' + options.hash['ur\"l'] + ')';
+        }");
+        $this->hb->registerHelper('helper3', "function () {
+            var options = arguments[arguments.length-1];
+            return options.fn(['test1', 'test2', 'test3']);
+        }");
+        $this->hb->registerHelper('helper4', "function () {
+            var options = arguments[arguments.length-1];
+
+            if (typeof options.hash.val !== 'undefined') {
+                this.helper4_value = options.hash.val % 2;
+                return options.fn(this);
+            }
+            if (typeof options.hash.odd !== 'undefined') {
+                return options.fn([1,3,5,7,9]);
+            }
+            return '';
+        }");
     }
 
-    public static function suite()
+    /**
+     * @dataProvider dataProvider
+     * @param $testName
+     * @param $tmpl
+     * @param $context
+     * @param $expected
+     */
+    public function testZordius($baseName, $tmpl, $context, $expected)
     {
-        return new self();
-    }
+        $template = $this->hb->compile($tmpl);
+        $actual = $template($context);
 
-
-    public function run(TestResult $result = null)
-    {
-        if ($result === null) {
-            $result = new TestResult();
+        if ($this->allowArrayObjectTypes) {
+            // V8Js objects have a different string representation. If these appear in the output there
+            // is something wrong with the template in the first place: they are safe to ignore
+            $actual = str_replace('[object Array]', '[object Object]', $actual);
         }
 
-        foreach ($this->tests as $json) {
-            $result->startTest($this);
-            Timer::start();
-            $stopTime = null;
-
-            $baseName = $tmpl = $context = $actual = '';
-            try {
-                preg_match('/(.*)(-[0-9]*).json$/', $json, $matches);
-                $base = $matches[1] . $matches[2];
-                $baseName = basename($base);
-                $dirName = dirname($json);
-
-                $hb = new Handlebars();
-                $hb->registerPartial([
-                    'partial1' => file_get_contents($dirName . '/partial001.tmpl'),
-                    'partial001' => file_get_contents($dirName . '/partial001.tmpl'),
-                    'partial2' => file_get_contents($dirName . '/partial002.tmpl'),
-                    '001-simple-vars' => file_get_contents($dirName . '/001-simple-vars.tmpl'),
-                    '017-hb-with' => file_get_contents($dirName . '/017-hb-with.tmpl')
-                ]);
-                $hb->registerHelper('helper1', "function (url, text) {
-                    return '<a href=\"' + url + '\">' + text + '</a>';
-                }");
-                $hb->registerHelper('helper2', "function (options) {
-                    return '<a href=\"' + options.hash.url + '\">' + options.hash.text + '</a>(' + options.hash['ur\"l'] + ')';
-                }");
-                $hb->registerHelper('helper3', "function () {
-                    var options = arguments[arguments.length-1];
-                    return options.fn(['test1', 'test2', 'test3']);
-                }");
-                $hb->registerHelper('helper4', "function () {
-                    var options = arguments[arguments.length-1];
-
-                    if (typeof options.hash.val !== 'undefined') {
-                        this.helper4_value = options.hash.val % 2;
-                        return options.fn(this);
-                    }
-                    if (typeof options.hash.odd !== 'undefined') {
-                        return options.fn([1,3,5,7,9]);
-                    }
-                    return '';
-                }");
-                $tmpl = file_get_contents($matches[1] . '.tmpl');
-                $context = json_decode(file_get_contents($json), true);
-                $expected = file_get_contents($base . '.txt');
-
-                $template = $hb->compile($tmpl);
-                $actual = $template($context);
-
-                if ($this->allowArrayObjectTypes) {
-                    // V8Js objects have a different string representation. If these appear in the output there
-                    // is something wrong with the template in the first place: they are safe to ignore
-                    $actual = str_replace('[object Array]', '[object Object]', $actual);
-                }
-
-                Assert::assertEquals($expected, $actual, "'$baseName' failed");
-            }
-            catch (AssertionFailedError $e) {
-                $stopTime = Timer::stop();
-                $result->addFailure($this, $e, $stopTime);
-            }
-            catch (\V8JsScriptException $e) {
-                $stopTime = Timer::stop();
-                $result->addFailure($this, new AssertionFailedError("'$baseName' failed", 0, $e), $stopTime);
-            }
-            catch (\Exception $e) {
-                $stopTime = Timer::stop();
-                $result->addError($this, $e, $stopTime);
-            }
-
-            if ($stopTime === null) {
-                $stopTime = Timer::stop();
-            }
-
-            $result->endTest($this, $stopTime);
-        }
-
-        return $result;
+        Assert::assertEquals($expected, $actual, "'$baseName' failed");
     }
 
-    public function count()
+
+    public function dataProvider()
     {
-        return 1;
+        $fixtureDir = __DIR__ . '/zordius';
+        $tests = [];
+        foreach (glob($fixtureDir . '/*.json') as $json) {
+            preg_match('/(.*)(-[0-9]*).json$/', $json, $matches);
+            $base = $matches[1] . $matches[2];
+            $baseName = basename($base);
+
+            $tests[] = [
+                $baseName,
+                file_get_contents($matches[1] . '.tmpl'),
+                json_decode(file_get_contents($json), true),
+                file_get_contents($base . '.txt')
+            ];
+        }
+        return $tests;
     }
 }
